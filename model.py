cce4c86ae07724caa18e95c72c33406ad1996c34
Nanzvx <anandacbca@gmail.com>
2025-06-09 12:15:10 +0530

    Decoder block is added

--- model.py
+++ model.py
@@ -138,11 +138,27 @@ class Encoder(nn.Module):
     def __init__(self, layers:nn.ModuleList) -> None:
         super().__init__()
         self.layers = layers
-        self.norm = LayerNormalization()
+        self.norm = LayerNormaclass DecoderBlock(nn.Module):
+    
+    def __init__(self, self_attention_block:MultiHeadAttentionBlock,cross_attetnion_block:MultiHeadAttentionBlock,feed_forward_block:FeedForwardblock,dropout:float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attetnion_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.Module(ResidualConnectionBlock(dropout) for _ in range(3))
+
+
+    def forward(self,x,encoder_out,src_mask,tgt_mask):
+
+        x = self.residual_connections[0](x,lambda x: self.self_attention_block(x,x,x,tgt_mask))
+        x = self.residual_connections[1](x,lambda x: self.cross_attention_block(x,encoder_out,encoder_out,src_mask))
+        x = self.residual_connections[2](x,self.feed_forward_block)
+        return xlization()
 
     def forward(self,x,mask):
         
         for layers in self.layers:
             x = layers(x,mask)
         return self.norm(x)
-    
+
+
